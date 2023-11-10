ca5b97782b2c17dad6e461d61ed78472635bfa25
Tom Schammo <tom@tomschammo.com>
2023-11-10 15:09:40 +0100

    Extend example to test `translate` function

--- LidarAug/augmentation.py
+++ LidarAug/augmentation.py
@@ -1,5 +1,7 @@
 import torch
-#from LidarAug import transformations
-import LidarAug
+from LidarAug import transformations
+# import LidarAug
 pc = torch.rand([1, 10000, 3])
+print(pc)
+transformations.translate(pc, torch.tensor([1.0, 2.0, 3.0]))
 print(pc)
