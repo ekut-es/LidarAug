13dff6726ffba66e12be5198d2574509cabf6462
Sven Teufel <sven.teufel@uni-tuebingen.de>
2023-11-10 12:29:21 +0100

    fix build

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
-#from pybind11.setup_helpers import Pybind11Extension, build_ext
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 from setuptools import setup
 
-__version__ = "0.0.1"
 
 ext_modules = [
-    CppExtension(name="transformations", sources=["LidarAug/transformations.cpp"]),
+    CppExtension(name="LidarAug.transformations", sources=["LidarAug/transformations.cpp"]),
 ]
 
 setup(
-    name='LidarAug',
-    version=__version__,
-    install_requires=[
-        'torch',
-    ],
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
-    zip_safe=False,
 )
