412af46d56deccc718f724cef21b24a0f2f7999d
Tom Schammo <tom@tomschammo.com>
2023-11-15 18:17:23 +0100

    feat: Add function to get truncated normal

--- LidarAug/cpp/include/transformations.hpp
+++ LidarAug/cpp/include/transformations.hpp
@@ -2,6 +2,8 @@
 #ifndef TRANSFORMATIONS_HPP
 #define TRANSFORMATIONS_HPP
 
+#include <boost/math/distributions/normal.hpp>
+#include <optional>
 #include <random>
 #include <torch/serialize/tensor.h>
 
@@ -29,6 +31,40 @@ inline double get_normal(double scale, double mean) {
   std::normal_distribution<double> dist(mean, scale);
 
   return dist(gen);
+}
+
+inline double get_truncated_normal_value(std::optional<double> mean = 0,
+                                         std::optional<double> sd = 1,
+                                         std::optional<double> low = 0,
+                                         std::optional<double> up = 10) {
+
+  // seed
+  std::random_device d;
+
+  // random number generator
+  std::mt19937 gen(d());
+
+  // create normal distribution
+  boost::math::normal_distribution<double> nd(mean.value(), sd.value());
+
+  // get upper and lower bounds using the cdf, which are the probabilities for
+  // the values being within those bounds
+  double lower_cdf = boost::math::cdf(nd, low.value());
+  double upper_cdf = boost::math::cdf(nd, up.value());
+
+  // create uniform distribution based on those bounds, plotting the
+  // probabilities
+  std::uniform_real_distribution<double> ud(lower_cdf, upper_cdf);
+
+  // sample uniform distribution, returning a uniformly distributed value
+  // between upper and lower
+  double ud_sample = ud(gen);
+
+  // use the quantile function (inverse of cdf, so equal to ppf) to 'convert'
+  // the sampled probability into its corresponding value
+  double sample = boost::math::quantile(nd, ud_sample);
+
+  return sample;
 }
 
 #endif // !TRANSFORMATIONS_HPP
