0d184e0fd190ca6b756c07f7f40a9f52939234c4
Tom Schammo <tom@tomschammo.com>
2023-11-15 19:53:03 +0100

    feat: Add bindings for the scale and flip functions
    For details see commits:
    
    - a6d0782
    - b452e32
    - a9ecd40

--- LidarAug/cpp/include/bindings.hpp
+++ LidarAug/cpp/include/bindings.hpp
@@ -9,4 +9,10 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def(
       "translate_random", &translate_random,
       "random translation function for point clouds and bounding boxes in C++");
+  m.def("scale_points", &scale_points, "function to scale point clouds in C++");
+  m.def("scale_labels", &scale_labels, "function to scale labels in C++");
+  m.def("scale_random", &scale_random,
+        "function to randomly scale point clouds and bounding boxes in C++");
+  m.def("flip_random", &flip_random,
+        "function to randomly flip point clouds and bounding boxes in C++");
 }
