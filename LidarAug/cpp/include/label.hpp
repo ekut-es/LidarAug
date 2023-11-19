fe94ce94ae06b6475ac8297544be32b9c4a17788
Tom Schammo <tom@tomschammo.com>
2023-11-19 12:58:29 +0100

    feat: Add header files to better represent labels and point clouds
    These header files enable me to clean up code to remove magic numbers,
    thereby making the code more readable.

---
+++ LidarAug/cpp/include/label.hpp
@@ -0,0 +1,25 @@
+
+#ifndef LABEL_HPP
+#define LABEL_HPP
+
+/**
+ * A label tensor represents a box and has the following values:
+ *
+ * x:     The x coordinate
+ * y:     The y coordinate
+ * z:     The z coordinate
+ * w:     The width of the box
+ * h:     The height of the box
+ * l:     The length of the box
+ * Theta: The rotation angle
+ */
+
+#define LABEL_X_IDX 0
+#define LABEL_Y_IDX 1
+#define LABEL_Z_IDX 2
+#define LABEL_W_IDX 3
+#define LABEL_H_IDX 4
+#define LABEL_L_IDX 5
+#define LABEL_ANGLE_IDX 6
+
+#endif // !LABEL_HPP
---
+++ LidarAug/cpp/include/point_cloud.hpp
@@ -0,0 +1,18 @@
+
+
+#ifndef POINT_CLOUD_HPP
+#define POINT_CLOUD_HPP
+
+/**
+ * A point cloud tensor represents a point cloud and has the following values:
+ *
+ * x: The x coordinate
+ * y: The y coordinate
+ * z: The z coordinate
+ */
+
+#define POINT_CLOUD_X_IDX 0
+#define POINT_CLOUD_Y_IDX 1
+#define POINT_CLOUD_Z_IDX 2
+
+#endif // !POINT_CLOUD_HPP
