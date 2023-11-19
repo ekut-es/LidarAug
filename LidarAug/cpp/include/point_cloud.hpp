58f847338b3a925e6e5504eb1771d93b82786db9
Tom Schammo <tom@tomschammo.com>
2023-11-19 20:26:38 +0100

    fix: Add intensity component to point cloud representation

--- LidarAug/cpp/include/point_cloud.hpp
+++ LidarAug/cpp/include/point_cloud.hpp
@@ -9,10 +9,12 @@
  * x: The x coordinate
  * y: The y coordinate
  * z: The z coordinate
+ * i: The intencity
  */
 
 #define POINT_CLOUD_X_IDX 0
 #define POINT_CLOUD_Y_IDX 1
 #define POINT_CLOUD_Z_IDX 2
+#define POINT_CLOUD_I_IDX 3
 
 #endif // !POINT_CLOUD_HPP
