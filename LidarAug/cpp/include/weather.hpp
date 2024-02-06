949f00c2f1250ab06728cecd502d0c0e9de3107a
Tom Schammo <tom@tomschammo.com>
2024-02-06 01:11:56 +0100

    feat: Add header for `fog` augmentation

--- LidarAug/cpp/include/weather.hpp
+++ LidarAug/cpp/include/weather.hpp
@@ -2,4 +2,12 @@
 #ifndef WEATHER_HPP
 #define WEATHER_HPP
 
+#include <cstddef>
+#include <torch/serialize/tensor.h>
+
+typedef enum { DIST, CHAMFER } fog_metric;
+
+void fog(at::Tensor point_cloud, std::size_t prob, fog_metric metric,
+         float sigma, int mean);
+
 #endif // !WEATHER_HPP
