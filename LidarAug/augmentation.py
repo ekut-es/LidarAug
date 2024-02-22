141c88243dc5f1d76b7e51c19a645c12a5dfa34f
Tom Schammo <tom@tomschammo.com>
2024-02-22 21:33:00 +0100

    refactor: Remove unused file

--- LidarAug/augmentation.py
+++
@@ -1,7 +0,0 @@
-import torch
-from LidarAug import transformations
-# import LidarAug
-pc = torch.rand([1, 10000, 3])
-print(pc)
-transformations.translate(pc, torch.tensor([1.0, 2.0, 3.0]))
-print(pc)
