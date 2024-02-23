f33f7b8464f67f6c9ece0cd8120db7a2622a579c
Tom Schammo <tom@tomschammo.com>
2024-02-23 11:47:12 +0100

    docs(stub): Add data for functions from the `transformations` module
    - `random_noise`
    - `thin_out`
    - `rotate_deg`
    - `rotate_rad`
    - `rotate_random`
    - `delete_labels_by_min_points`
    - `random_point_noise`
    - `transform_along_ray`
    - `intensity_noise`
    - `intensity_shift`

--- src/LidarAug/transformations.pyi
+++ src/LidarAug/transformations.pyi
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Tuple
 from torch import Tensor
 
 
@@ -111,5 +112,130 @@ def flip_random(points: Tensor, labels: Tensor, prob: int) -> None:
     :param points:  is the point cloud containing the points that will be flipped.
     :param labels:  are the corresponding labels.
     :param prob:    is the probability with which the points should be flipped.
+    """
+    ...
+
+
+def random_noise(points: Tensor, sigma: float,
+                 ranges: list[float] | DistributionRanges,
+                 noise_type: NoiseType, max_intensity: ItensityRange) -> None:
+    """
+    Adds random amount of points (drawn using a normal distribution) at random coordinates
+    (within predetermined ranges) with a random intensity according to specific noise type.
+
+    :param points:         is the point cloud that the points will be added to.
+    :param sigma:          is the standard deviation of the normal distribution that is used to draw the number of points to be added.
+    :param ranges:         are the boundaries in (min and max (x, y, z) values) in which the new points can be created.
+    :param noise_type:     is one of a number of 'patterns' that can be used to generate the points.
+    :param max_intensity:  is the maximum intensity value in the dataset
+    """
+    ...
+
+
+def thin_out(points: Tensor, sigma: float) -> Tensor:
+    """
+     Randomly genereates a percentage from a norma distribution, which determines
+     how many items should be 'thinned out'. From that percentage random indeces
+     are uniformly drawn (in a random order, where each index is unique).
+
+     Finally a new tensor is created containing the items present at those
+     indeces.
+
+    :param points: is the point cloud.
+    :param sigma:  is the standard diviation of the distribution that genereates the percentage.
+    """
+    ...
+
+
+def rotate_deg(points: Tensor, angle: float) -> None:
+    """
+    Rotates a batch of points anlong the 'z' axis (yaw).
+
+    :param points: is the point cloud that the rotation is applied to.
+    :param angle:  is the angle (in degrees) by which the points are to be rotated.
+    """
+    ...
+
+
+def rotate_rad(points: Tensor, angle: float) -> None:
+    """
+    Rotates a batch of points anlong the 'z' axis (yaw).
+
+    :param points: is the point cloud that the rotation is applied to.
+    :param angle:  is the angle (in radians) by which the points are to be rotated.
+    """
+    ...
+
+
+def rotate_random(points: Tensor, labels: Tensor, sigma: float) -> None:
+    """
+    Rotates points and labels.
+    The number of degrees that they are rotated by is determined by a randomly genereated value from a normal distribution.
+
+    :param points: is the point cloud that the rotation is applied to.
+    :param labels: are the labels belonging to the point cloud that the rotation is applied to.
+    :param sigma:  is the standard deviation of the normal distribution.
+    """
+    ...
+
+
+def delete_labels_by_min_points(
+        points: Tensor, labels: Tensor, names: Tensor,
+        min_points: int) -> Tuple[list[Tensor], list[Tensor]]:
+    """
+     Checks the amount of points for each bounding box.
+     If the number of points is smaller than a given threshold, the box is removed
+     along with its label.
+
+    :param points:     is the point_cloud.
+    :param labels:     are the bounding boxes of objects.
+    :param names:      are the names/labels of these boxes.
+    :param min_points: is the point threshold.
+    """
+    ...
+
+
+def random_point_noise(points: Tensor, sigma: float):
+    """
+    Moves each point in the point cloud randomly.
+    How much each coordinate is changed is decided by values drawn from a normal distribution.
+
+    :param points: is the point cloud from which each point is moved.
+    :param sigma:  is the standard diviation of the normal distribution.
+    """
+    ...
+
+
+def transform_along_ray(points: Tensor, sigma: float):
+    """
+    Moves each point in the point cloud randomly along a ray.
+    How much it is moved is decided by a value drawn from a normal distribution.
+
+    :param points: is the point cloud from which each point is moved.
+    :param sigma:  is the standard diviation of the normal distribution.
+    """
+    ...
+
+
+def intensity_noise(points: Tensor, sigma: float,
+                    max_intensity: ItensityRange):
+    """
+    Shifts the intensity value of every point in the point cloud by a random amount drawn from a normal distribution.
+
+    :param points:        is the point cloud with all the points.
+    :param sigma:         is the standard diviation of the normal distribution.
+    :param max_intensity: is the maximum intensity value (either 1 or 255, depending on the dataset).
+    """
+    ...
+
+
+def intensity_shift(points: Tensor, sigma: float,
+                    max_intensity: ItensityRange):
+    """
+    Shifts the intensity value of every point in the point cloud by a single value drawn from a normal distribution.
+
+    :param points:        is the point cloud with all the points.
+    :param sigma:         is the standard diviation of the normal distribution.
+    :param max_intensity: is the maximum intensity value (either 1 or 255, depending on the dataset).
     """
     ...
