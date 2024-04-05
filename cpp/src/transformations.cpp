e887cd413ca1915b830ae1513dc27b6ab7a172be
Tom Schammo <tom@tomschammo.com>
2024-04-05 11:53:52 +0200

    perf: Increase performance of `translate`
    Increase performance of `translate` by 3400-3700x.

--- cpp/src/transformations.cpp
+++ cpp/src/transformations.cpp
@@ -12,23 +12,7 @@
 using Slice = torch::indexing::Slice;
 
 void translate(at::Tensor points, const at::Tensor &translation) {
-  const dimensions dims = {points.size(0), points.size(1), points.size(2)};
-  const float *const translation_ptr = translation.data_ptr<float>();
-  const float x_translation =
-      translation_ptr[0]; // NOLINT: Allow pointer arithmetic to access contents
-  const float y_translation =
-      translation_ptr[1]; // NOLINT: Allow pointer arithmetic to access contents
-  const float z_translation =
-      translation_ptr[2]; // NOLINT: Allow pointer arithmetic to access contents
-
-  // translate all point clouds in a batch by the same amount
-  for (tensor_size_t i = 0; i < dims.batch_size; i++) {
-    for (tensor_size_t j = 0; j < dims.num_items; j++) {
-      points.index({i, j, POINT_CLOUD_X_IDX}) += x_translation;
-      points.index({i, j, POINT_CLOUD_Y_IDX}) += y_translation;
-      points.index({i, j, POINT_CLOUD_Z_IDX}) += z_translation;
-    }
-  }
+  points.index({Slice(), Slice(), Slice(0, 3)}) += translation;
 }
 
 void scale_points(at::Tensor points, float factor) {
