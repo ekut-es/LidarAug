3f2521b41e6a7b8797ebee07ffeaf564f7c69c3e
Tom Schammo <tom@tomschammo.com>
2024-04-05 10:47:59 +0200

    feat: Add header of `intersects` function
    Related to issue #23.

--- cpp/include/raytracing.hpp
+++ cpp/include/raytracing.hpp
@@ -11,4 +11,11 @@
                                   torch::Tensor split_index,
                                   std::optional<float> intensity_factor = 0.9);
 
+void intersects(torch::Tensor point_cloud, torch::Tensor noise_filter,
+                torch::Tensor split_index, torch::Tensor intersections,
+                torch::Tensor distances, torch::Tensor distance_count,
+                torch::Tensor most_intersect_count,
+                torch::Tensor most_intersect_dist, tensor_size_t num_points,
+                float intensity_factor);
+
 #endif // !RAYTRACING_HPP
