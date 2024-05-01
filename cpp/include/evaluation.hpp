7894c517b681c5297092a1ac12e534d309407eaa
Tom Schammo <tom@tomschammo.com>
2024-05-01 19:21:08 +0200

    feat: Add shorter custom result type
    Wrap the result map into a custom type to make the typing shorter and
    keep it more abstract.

--- cpp/include/evaluation.hpp
+++ cpp/include/evaluation.hpp
@@ -2,4 +2,12 @@
 #ifndef EVALUATION_HPP
 #define EVALUATION_HPP
 
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+template <typename T> struct result_dict {
+  typedef std::map<float, std::unordered_map<std::string, std::vector<T>>> type;
+};
 #endif // !EVALUATION_HPP
