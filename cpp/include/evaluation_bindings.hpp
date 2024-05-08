8c34f2265afb14d8f54f5f57d6a35a62fcf8a00e
Tom Schammo <tom@tomschammo.com>
2024-05-08 14:03:19 +0200

    fix: Adjust bindings

--- cpp/include/evaluation_bindings.hpp
+++ cpp/include/evaluation_bindings.hpp
@@ -2,7 +2,7 @@
 #include <pybind11/pybind11.h>
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("evaluate", &evaluate_results<float>);
+  m.def("evaluate", &evaluate_results);
   m.def("calculate_false_and_true_positive",
         &calculate_false_and_true_positive);
 }
