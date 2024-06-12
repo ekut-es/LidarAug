c321491c27acd3fcf7eb6f79f0984fca3413fa03
Tom Schammo <tom@tomschammo.com>
2024-06-12 13:12:26 +0200

    fix: Use `std::vector` instead of `torch::List`
    Use `std::vector` instead of `torch::List` because there is no
    conversion for `torch::List` into a Python type.

--- cpp/include/weather.hpp
+++ cpp/include/weather.hpp
@@ -2,13 +2,13 @@
 #ifndef WEATHER_HPP
 #define WEATHER_HPP
 
-#include <ATen/core/List.h>
 #include <optional>
 #include <torch/serialize/tensor.h>
+#include <vector>
 
 typedef enum { DIST, CHAMFER } fog_parameter;
 
-[[nodiscard]] std::optional<torch::List<torch::Tensor>>
+[[nodiscard]] std::optional<std::vector<torch::Tensor>>
 fog(const torch::Tensor &point_cloud, float prob, fog_parameter metric,
     float sigma, int mean);
 
