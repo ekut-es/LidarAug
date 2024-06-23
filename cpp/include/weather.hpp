a646c6f273026930e7dc26bef526773eaa2e1f25
Tom Schammo <tom@tomschammo.com>
2024-06-23 17:22:46 +0200

    feat: Implement `universal_weather`
    Add provisional implementation for `universal_weather` that needs review
    to make sure everything is correct and as intended.

--- cpp/include/weather.hpp
+++ cpp/include/weather.hpp
@@ -27,5 +27,9 @@ fog(const torch::Tensor &point_cloud, float prob, fog_parameter metric,
                                  std::array<float, 6> dims, uint32_t num_drops,
                                  float precipitation, int32_t scale,
                                  float max_intensity);
+void universal_weather(torch::Tensor point_cloud, float prob, float sigma,
+                       int mean, float ext_a, float ext_b, float beta_a,
+                       float beta_b, float del_a, float del_b, int int_a,
+                       int int_b, int mean_int, int int_range);
 
 #endif // !WEATHER_HPP
--- cpp/src/weather.cpp
+++ cpp/src/weather.cpp
@@ -191,6 +191,68 @@ fog(const torch::Tensor &point_cloud, float prob, fog_parameter metric,
   return point_cloud;
 }
 
+void universal_weather(torch::Tensor point_cloud, float prob, float sigma,
+                       int mean, float ext_a, float ext_b, float beta_a,
+                       float beta_b, float del_a, float del_b, int int_a,
+                       int int_b, int mean_int, int int_range) {
+
+  auto rng = get_rng();
+  std::uniform_real_distribution<float> distrib(0, HUNDRED_PERCENT - 1);
+  auto rand = distrib(rng);
+
+  if (prob > rand) {
+    const auto viewing_dist = get_truncated_normal_value(mean, sigma, 0, mean);
+
+    const auto extinction_factor = ext_a * exp(ext_b * viewing_dist);
+
+    const auto beta = (-beta_a * viewing_dist) + beta_b;
+    const auto delete_probability = -del_a * exp(-del_b * viewing_dist) + 1;
+
+    // selecting points for modification and deletion
+    const auto dist = torch::sqrt(
+        torch::sum(point_cloud.index({Slice(), Slice(None, 3)}).pow(2), 1));
+    const auto modify_probability = 1 - torch::exp(-extinction_factor * dist);
+    const auto modify_threshold = torch::rand(modify_probability.size(0));
+    const auto selected = modify_threshold < modify_probability;
+    const auto delete_threshold = torch::rand(point_cloud.size(0));
+    const auto deleted =
+        torch::logical_and(delete_threshold < delete_probability, selected);
+
+    // changing intensity of unaltered points according to parametrized beer
+    // lambert law
+    point_cloud.index({torch::logical_not(selected), 3}) *=
+        int_a * torch::exp(-(int_b / viewing_dist) *
+                           dist.index({torch::logical_not(selected)}));
+
+    // changing position and intensity of selected points
+    const auto altered_points =
+        torch::logical_and(selected, torch::logical_not(deleted));
+    const auto num_altered_points =
+        point_cloud.index({altered_points, Slice(None, 3)}).size(0);
+    if (num_altered_points > 0) {
+
+      auto newdist =
+          torch::empty(num_altered_points).exponential_(1 / beta) + 1.3;
+
+      point_cloud.index_put_(
+          {altered_points, Slice(None, 3)},
+          point_cloud.index({altered_points, Slice(None, 3)}) *
+              torch::reshape(newdist / dist.index({altered_points}), {-1, 1}));
+
+      const auto min_int = std::max(mean_int - (int_range / 2), 0);
+      const auto max_int = std::min(mean_int + (int_range / 2), 255);
+
+      point_cloud.index_put_(
+          {altered_points, 3},
+          torch::empty({num_altered_points}).uniform_(min_int, max_int));
+    }
+
+    // delete points
+    point_cloud = point_cloud.index({torch::logical_not(deleted), Slice()});
+    point_cloud.index({point_cloud.index({Slice(), 3}) > 255, 3}) = 255;
+  }
+}
+
 #ifdef BUILD_MODULE
 #undef TEST_RNG
 #include "../include/weather_bindings.hpp"
