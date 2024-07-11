6ab516a51eb01fff21e059847716dfaaf84cd1ab
Tom Schammo <tom@tomschammo.com>
2024-07-11 12:18:16 +0200

    refactor(inverted_*): Improve const-correctness and adhere to naming standards

--- cpp/include/stats.hpp
+++ cpp/include/stats.hpp
@@ -176,23 +176,23 @@ draw_unique_uniform_values(std::size_t size, std::size_t num_values) {
 }
 
 [[nodiscard]] inline torch::Tensor
-inverted_lognormal_cdf(const torch::Tensor &D, float R) {
-  return std::pow(R, 0.23) *
-         torch::exp((std::numbers::sqrt2 * std::log(1.43 - (0.0003 * R)) *
-                     torch::erfinv((0.0116279 * D) / (std::pow(R, 0.22)))) -
+inverted_lognormal_cdf(const torch::Tensor &d, const float r) {
+  return std::pow(r, 0.23) *
+         torch::exp((std::numbers::sqrt2 * std::log(1.43 - (0.0003 * r)) *
+                     torch::erfinv((0.0116279 * d) / (std::pow(r, 0.22)))) -
                     0.328504);
 }
 
 [[nodiscard]] inline torch::Tensor
-inverted_exponential_cdf(const torch::Tensor &D, float R) {
-  return -0.243902 * std::pow(R, 0.21) *
-         torch::log(0.0005124998718750320 * D * std::pow(R, -0.21));
+inverted_exponential_cdf(const torch::Tensor &d, const float r) {
+  return -0.243902 * std::pow(r, 0.21) *
+         torch::log(0.0005124998718750320 * d * std::pow(r, -0.21));
 }
 
 [[nodiscard]] inline torch::Tensor
-inverted_exponential_gm(const torch::Tensor &D, float R) {
-  return -0.436681 * std::pow(R, 0.48) *
-             torch::log(0.000916002564807181 * D * std::pow(R, 0.46)) -
+inverted_exponential_gm(const torch::Tensor &d, const float r) {
+  return -0.436681 * std::pow(r, 0.48) *
+             torch::log(0.000916002564807181 * d * std::pow(r, 0.46)) -
          5.9143581981431375;
 }
 
