dfba4631fd98a38d5b7d15386985e142c666ae80
Tom Schammo <tom@tomschammo.com>
2024-07-11 15:16:19 +0200

    fix: Update weather bindings
    Update weather bindings to reflect the transition from enum to enum
    struct.

--- cpp/include/weather_bindings.hpp
+++ cpp/include/weather_bindings.hpp
@@ -7,7 +7,7 @@
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("fog", &fog, "fog weather simulation");
   pybind11::enum_<fog_parameter>(m, "FogParameter")
-      .value("DIST", DIST)
-      .value("CHAMFER", CHAMFER)
+      .value("DIST", fog_parameter::DIST)
+      .value("CHAMFER", fog_parameter::CHAMFER)
       .export_values();
 }
