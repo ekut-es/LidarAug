795218d72881643fdbeaf957ef921c34b4d66333
Tom Schammo <tom@tomschammo.com>
2024-08-27 12:02:14 +0200

    docs: Add doc string for the `snow` simulation
    Fixes part of issue #10.

--- src/LidarAug/weather_simulations.pyi
+++ src/LidarAug/weather_simulations.pyi
@@ -57,4 +57,15 @@ def rain(point_cloud: Tensor, dims: list[float], num_drops: int,
 def snow(point_cloud: Tensor, dims: list[float], num_drops: int,
          precipitation: float, scale: int,
          max_intensity: IntensityRange) -> Tensor:
+    """
+    Applies a snow simulation to a point cloud.
+
+    :param point_cloud: is the point cloud that the simulation is applied to.
+    :param dims: set the upper and lower bounds of the uniform distribution used to draw new points for the noise filter.
+    :param num_drops: is the number of snow flakes per m^3.
+    :param precipitation: is the precipitation rate and determines the snowflake size distribution.
+    :param scale: is used to scale the size of the sampled particles when generating the noise filter.
+    :param max_intensity: is the maximum intensity of the points in the point cloud.
+    :return: a new point cloud with the old points as a base but after applying the simulation.
+    """
     ...
