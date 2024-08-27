ff673be2d2e69bd04f4b65f24bcba53b757c2dcc
Tom Schammo <tom@tomschammo.com>
2024-08-27 12:01:04 +0200

    docs: Add doc string for `rain` simulation
    Fixes part of issue #10.

--- src/LidarAug/weather_simulations.pyi
+++ src/LidarAug/weather_simulations.pyi
@@ -40,6 +40,17 @@ def fog(point_cloud: Tensor, metric: FogParameter, viewing_dist: float,
 def rain(point_cloud: Tensor, dims: list[float], num_drops: int,
          precipitation: float, d: Distribution,
          max_intensity: IntensityRange) -> Tensor:
+    """
+    Applies a rain simulation to a point cloud.
+
+    :param point_cloud: is the point cloud that the simulation is applied to.
+    :param dims: set the upper and lower bounds of the uniform distribution used to draw new points for the noise filter.
+    :param num_drops: is the number of rain drops per m^3.
+    :param precipitation: is the precipitation rate and determines the raindrop size distribution.
+    :param d: is the distribution function used when sampling the particles.
+    :param max_intensity: is the maximum intensity of the points in the point cloud.
+    :return: a new point cloud with the old points as a base but after applying the simulation.
+    """
     ...
 
 
