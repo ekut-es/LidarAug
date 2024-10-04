f0e2e20aceaa0e1cc84ca0b9e7a44a7a93e4c456
Tom Schammo <tom@tomschammo.com>
2024-10-04 16:40:21 +0200

    refactor: Improve code (by applying DRY principles)
    Move C++ standard version and compiler optimization level out.

--- setup.py
+++ setup.py
@@ -3,23 +3,27 @@ from setuptools import setup
 
 MODULE_NAME = "LidarAug"
 
+CPP_VERSION = "-std=c++20"
+COMPILER_OPTIMIZATION_LEVEL = "-O3"
+
 ext_modules = [
     CppExtension(
         name=f"{MODULE_NAME}.transformations",
         sources=["cpp/src/transformations.cpp", "cpp/src/tensor.cpp"],
         define_macros=[("BUILD_MODULE", None)],
-        extra_compile_args=['-std=c++20', '-O3'],
+        extra_compile_args=[CPP_VERSION, COMPILER_OPTIMIZATION_LEVEL],
     ),
     CppExtension(
         name=f"{MODULE_NAME}.weather_simulations",
         sources=["cpp/src/weather.cpp"],
         define_macros=[("BUILD_MODULE", None)],
-        extra_compile_args=['-std=c++20', '-O3'],
+        extra_compile_args=[CPP_VERSION, COMPILER_OPTIMIZATION_LEVEL],
     ),
     CppExtension(name=f"{MODULE_NAME}.evaluation",
                  sources=["cpp/src/evaluation.cpp", "cpp/src/utils.cpp"],
                  define_macros=[("BUILD_MODULE", None)],
-                 extra_compile_args=['-std=c++20', '-O3']),
+                 extra_compile_args=[CPP_VERSION,
+                                     COMPILER_OPTIMIZATION_LEVEL]),
 ]
 
 setup(
