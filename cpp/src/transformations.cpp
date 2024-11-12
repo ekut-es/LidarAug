5ed7b9293e21f70cc74bc260dcdda2498465849f
Tom Schammo <tom@tomschammo.com>
2024-11-12 14:51:22 +0100

    perf: Rewrite `transform_along_ray`
    Make use of torch native operations to increase performance (up to ~167x in some instances).

--- cpp/src/transformations.cpp
+++ cpp/src/transformations.cpp
@@ -374,22 +374,10 @@ void random_point_noise(torch::Tensor points, const float sigma) {
 void transform_along_ray(torch::Tensor points, const float sigma) {
   const dimensions dims = {points.size(0), points.size(1), points.size(2)};
 
-  std::normal_distribution<float> dist(0, sigma);
-
-  // TODO(tom): perf measure this
-  for (tensor_size_t i = 0; i < dims.batch_size; i++) {
-    for (tensor_size_t j = 0; j < dims.num_items; j++) {
-      auto *const v = points[i][j].data_ptr<float>();
-      const auto noise = std::get<VALUE>(draw_values<float>(dist));
-
-      // NOLINTBEGIN
-      // Allow pointer arithmetic for accessing tensor contents
-      v[0] += noise;
-      v[1] += noise;
-      v[2] += noise;
-      // NOLINTEND
-    }
-  }
+  auto noise = torch::normal(0, sigma, {dims.batch_size, dims.num_items, 1})
+                   .repeat({1, 1, 3});
+  points.index({torch::indexing::Slice(), torch::indexing::Slice(),
+                torch::indexing::Slice(0, 3)}) += noise;
 }
 
 void intensity_noise(torch::Tensor points, const float sigma,
