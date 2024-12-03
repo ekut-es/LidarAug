76be65a7ab8d389b2fbd0623b1a0e516d6ac1171
Tom Schammo <tom@tomschammo.com>
2024-12-03 02:44:54 +0100

    docs(point_cloud): Add docs to bindings
    Use the 'doc' parameter when creating the bindings to add documentation
    to the bindings.

--- cpp/include/point_cloud_bindings.hpp
+++ cpp/include/point_cloud_bindings.hpp
@@ -2,10 +2,21 @@
 #include <pybind11/pybind11.h>
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("set_max_intensity", &point_cloud_data::max_intensity::set);
-  m.def("get_max_intensity", &point_cloud_data::max_intensity::get);
+  m.def("set_max_intensity", &point_cloud_data::max_intensity::set,
+        "Set the global state tracker for the maximum intensity.\n\n\
+\
+:param val: is the new maximum intensity (member of `IntensityRange`).");
 
-  pybind11::enum_<point_cloud_data::intensity_range>(m, "IntensityRange")
+  m.def(
+      "get_max_intensity", &point_cloud_data::max_intensity::get,
+      "Get the current value of the maximum intensity global state tracker.\n\n\
+\
+:return: an int representing the maximum intensity value.");
+
+  pybind11::enum_<point_cloud_data::intensity_range>(
+      m, "IntensityRange",
+      "Defines options for maximum intensity values.\nIntensity goes from [0; "
+      "MAX_INTENSITY], where MAX_INTENSITY is either 1 or 255.")
       .value("MAX_INTENSITY_1",
              point_cloud_data::intensity_range::MAX_INTENSITY_1)
       .value("MAX_INTENSITY_255",
