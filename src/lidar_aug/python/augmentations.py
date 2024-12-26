fd948961eaaaad2b59415127e2a4ed50799b478b
Tom Schammo <tom@tomschammo.com>
2024-12-26 14:26:41 +0100

    docs: Add information about the expected shape of input tensors

--- src/lidar_aug/python/augmentations.py
+++ src/lidar_aug/python/augmentations.py
@@ -57,7 +57,7 @@ def translate(points: Tensor, translation: Tensor) -> None:
     """
     Moves points by a specific amount.
 
-    :param points:      is the point cloud with the points are to be moved.
+    :param points:      is the point cloud with the points are to be moved. Expected shape is `(B, N, 4)`.
     :param translation: is the translation vector that specifies by how much they points are moved.
     """
 
@@ -70,8 +70,8 @@ def translate_random(points: Tensor, labels: Tensor, sigma: float) -> None:
     """
     Generates a random (3D) translation vector using a normal distribution and applies it to all the points and labels.
 
-    :param points: is the point cloud with the points that are translated.
-    :param labels: are the labels belonging to the aforementioned point cloud.
+    :param points: is the point cloud with the points that are translated. Expected shape is `(B, N, 4)`.
+    :param labels: are the labels belonging to the aforementioned point cloud. Expected shape is `(B, N, 7)`.
     :param sigma:  is the standard deviation of the normal distribution.
     """
 
@@ -83,10 +83,8 @@ def translate_random(points: Tensor, labels: Tensor, sigma: float) -> None:
 def scale(points: Tensor, scaling_factor: float) -> None:
     """
     Scales points by a constant factor.
-    Point cloud is expected to be of shape (b, n, f), where `b` is the number of batches, `n` is the number of points and `f` is the number of features.
-    `f` is supposed to be 4.
 
-    :param points:         is the point cloud whose points are scaled.
+    :param points:         is the point cloud whose points are scaled. Expected shape is `(B, N, 4)`.
     :param scaling_factor: is the factor that the (x, y, z) coordinates are multiplied by.
     """
     _check_points(points)
@@ -102,8 +100,8 @@ def scale_random(points: Tensor, labels: Tensor, sigma: float,
     The truncated normal distribution has a mean of 1. The standard deviation, as
     well as upper and lower limits are determined by the function parameters.
 
-    :param points:    is the point cloud that contains the points that will be scaled.
-    :param labels:    are the labels belonging to the aforementioned point cloud.
+    :param points:    is the point cloud that contains the points that will be scaled. Expected shape is `(B, N, 4)`.
+    :param labels:    are the labels belonging to the aforementioned point cloud. Expected shape is `(B, N, 7)`.
     :param sigma:     is the standard deviation of the truncated normal distribution.
     :param max_scale: is the upper limit of the truncated normal distribution. The lower limit is the inverse.
     """
@@ -123,8 +121,8 @@ def scale_local(points: Tensor, labels: Tensor, sigma: float,
     The truncated normal distribution has a mean of 1. The standard deviation, as
     well as upper and lower limits are determined by the function parameters.
 
-    :param points:    is the point cloud that contains the points that will be scaled.
-    :param labels:    are the labels belonging to the aforementioned point cloud.
+    :param points:    is the point cloud that contains the points that will be scaled. Expected shape is `(B, N, 4)`.
+    :param labels:    are the labels belonging to the aforementioned point cloud. Expected shape is `(B, N, 7)`.
     :param sigma:     is the standard deviation of the truncated normal distribution.
     :param max_scale: is the upper limit of the truncated normal distribution. The lower limit is the inverse.
     """
@@ -138,8 +136,8 @@ def flip_random(points: Tensor, labels: Tensor, prob: int) -> None:
     """
     Flips all the points in the point cloud with a probability of `prob` % in the direction of the y-axis.
 
-    :param points:  is the point cloud containing the points that will be flipped.
-    :param labels:  are the corresponding labels.
+    :param points:  is the point cloud containing the points that will be flipped. Expected shape is `(B, N, 4)`.
+    :param labels:  are the corresponding labels. Expected shape is `(B, N, 7)`.
     :param prob:    is the probability with which the points should be flipped.
     """
 
@@ -157,7 +155,7 @@ def random_noise(points: Tensor, sigma: float,
     Adds random amount of points (drawn using a normal distribution) at random coordinates
     (within predetermined ranges) with a random intensity according to specific noise type.
 
-    :param points:         is the point cloud that the points will be added to.
+    :param points:         is the point cloud that the points will be added to. Expected shape is `(B, N, 4)`.
     :param sigma:          is the standard deviation of the normal distribution that is used to draw the number of points to be added.
     :param ranges:         are the boundaries in (min and max (x, y, z) values) in which the new points can be created.
     :param noise_type:     is one of a number of 'patterns' that can be used to generate the points.
@@ -197,7 +195,7 @@ def thin_out(points: Tensor, sigma: float) -> None:
     Finally, a new tensor is created containing the items present at those
     indices.
 
-    :param points: is the point cloud.
+    :param points: is the point cloud. Expected shape is `(B, N, 4)`.
     :param sigma:  is the standard deviation of the distribution that generates the percentage.
     """
 
@@ -212,7 +210,7 @@ def rotate_deg(points: Tensor, angle: float) -> None:
     """
     Rotates a batch of points along the 'z' axis (yaw).
 
-    :param points: is the point cloud that the rotation is applied to.
+    :param points: is the point cloud that the rotation is applied to. Expected shape is `(B, N, 4)`.
     :param angle:  is the angle (in degrees) by which the points are to be rotated.
     """
 
@@ -225,7 +223,7 @@ def rotate_rad(points: Tensor, angle: float) -> None:
     """
     Rotates a batch of points along the 'z' axis (yaw).
 
-    :param points: is the point cloud that the rotation is applied to.
+    :param points: is the point cloud that the rotation is applied to. Expected shape is `(B, N, 4)`.
     :param angle:  is the angle (in radians) by which the points are to be rotated.
     """
 
@@ -239,8 +237,8 @@ def rotate_random(points: Tensor, labels: Tensor, sigma: float) -> None:
     Rotates points and labels.
     The number of degrees that they are rotated by is determined by a randomly generated value from a normal distribution.
 
-    :param points: is the point cloud that the rotation is applied to.
-    :param labels: are the labels belonging to the point cloud that the rotation is applied to.
+    :param points: is the point cloud that the rotation is applied to. Expected shape is `(B, N, 4)`.
+    :param labels: are the labels belonging to the point cloud that the rotation is applied to. Expected shape is `(B, N, 7)`.
     :param sigma:  is the standard deviation of the normal distribution.
     """
 
@@ -256,8 +254,8 @@ def delete_labels_by_min_points(points: Tensor, labels: Tensor, names: Tensor,
      If the number of points is smaller than a given threshold, the box is removed
      along with its label.
 
-    :param points:     is the point_cloud.
-    :param labels:     are the bounding boxes of objects.
+    :param points:     is the point_cloud. Expected shape is `(B, N, 4)`.
+    :param labels:     are the bounding boxes of objects. Expected shape is `(B, N, 7)`.
     :param names:      are the names/labels of these boxes.
     :param min_points: is the point threshold.
     """
@@ -279,7 +277,7 @@ def random_point_noise(points: Tensor, sigma: float) -> None:
     Moves each point in the point cloud randomly.
     How much each coordinate is changed is decided by values drawn from a normal distribution.
 
-    :param points: is the point cloud from which each point is moved.
+    :param points: is the point cloud from which each point is moved. Expected shape is `(B, N, 4)`.
     :param sigma:  is the standard deviation of the normal distribution.
     """
 
@@ -293,7 +291,7 @@ def transform_along_ray(points: Tensor, sigma: float) -> None:
     Moves each point in the point cloud randomly along a ray.
     How much it is moved is decided by a value drawn from a normal distribution.
 
-    :param points: is the point cloud from which each point is moved.
+    :param points: is the point cloud from which each point is moved. Expected shape is `(B, N, 4)`.
     :param sigma:  is the standard deviation of the normal distribution.
     """
 
@@ -307,7 +305,7 @@ def intensity_noise(points: Tensor, sigma: float,
     """
     Shifts the intensity value of every point in the point cloud by a random amount drawn from a normal distribution.
 
-    :param points:        is the point cloud with all the points.
+    :param points:        is the point cloud with all the points. Expected shape is `(B, N, 4)`.
     :param sigma:         is the standard deviation of the normal distribution.
     :param max_intensity: is the maximum intensity value (either 1 or 255, depending on the dataset).
     """
@@ -322,7 +320,7 @@ def intensity_shift(points: Tensor, sigma: float,
     """
     Shifts the intensity value of every point in the point cloud by a single value drawn from a normal distribution.
 
-    :param points:        is the point cloud with all the points.
+    :param points:        is the point cloud with all the points. Expected shape is `(B, N, 4)`.
     :param sigma:         is the standard deviation of the normal distribution.
     :param max_intensity: is the maximum intensity value (either 1 or 255, depending on the dataset).
     """
@@ -336,7 +334,7 @@ def local_to_world_transform(lidar_pose: Tensor) -> Tensor:
     """
     Creates a transformation matrix from the local system into the global coordinate frame.
 
-    :param lidar_pose: is the local coordinate frame (x, y, z, roll, yaw, pitch).
+    :param lidar_pose: is the local coordinate frame `(x, y, z, roll, yaw, pitch)`.
     :return: the homogeneous transformation matrix into the global coordinate frame.
     """
 
@@ -349,8 +347,8 @@ def local_to_local_transform(from_pose: Tensor, to_pose: Tensor) -> Tensor:
     """
     Creates a transformation matrix from the local system into a 'target' coordinate frame.
 
-    :param from_pose: is the local coordinate frame (x, y, z, roll, yaw, pitch).
-    :param to_pose:   is the target coordinate frame (x, y, z, roll, yaw, pitch).
+    :param from_pose: is the local coordinate frame `(x, y, z, roll, yaw, pitch)`.
+    :param to_pose:   is the target coordinate frame `(x, y, z, roll, yaw, pitch)`.
     :return: the homogeneous transformation matrix into the target coordinate frame.
     """
 
@@ -367,7 +365,7 @@ def apply_transformation(points: Tensor,
     N, F), where B is the number of batches and N is the number of points.
 
     :param points:                is the point cloud that the transformation
-                                  matrix is applied to.
+                                  matrix is applied to. Expected shape is `(B, N, 4)`.
     :param transformation_matrix: is the transformation matrix.
     """
 
